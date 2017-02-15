795e48a2db9e8b04ffdddb863b172682912cb4a0
miao jing <243127395@qq.com>
2017-02-15 10:48:18 +0800

    getEnvConfig throw error if config for target key not found

--- server/data/model/user.ts
+++ server/data/model/user.ts
@@ -51,7 +51,6 @@ export const User: any = bookshelf.Model.extend(
             return this.forge(data).save(null, options);
         },
 
-        // todo: make this func async
         /*
         find user , compare password, if not match return null else return user
         @return Promise<user|null, null>
@@ -92,7 +91,7 @@ export const User: any = bookshelf.Model.extend(
         // return a JWT token
         createTokenPr: async function (user: DBUser) {
             // todo: change getEnvConfig method to more secure one
-            return await jwtSign({ id: user.id }, ENV_UTILS.getEnvConfig().JWT_SIGNED_TOKEN, {})
+            return await jwtSign({ id: user.id }, ENV_UTILS.getEnvConfig('JWT_SIGNED_TOKEN'), {})
         }
 
     });
